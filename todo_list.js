fc4122bc9bf67d83b72f3a373f40176af0a42e97
Tayyeba0 <159903055+Tayyeba0@users.noreply.github.com>
2024-04-07 07:30:33 +0500

    my first commit

---
+++ todo_list.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todosquestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would you like to add in your todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: 'would you like to add more in your todos?',
+            default: "true"
+        }
+    ]);
+    if (todosquestions.firstQuestion == '') {
+        todos.push(todosquestions.firstQuestion);
+        console.log(todos);
+    }
+    else {
+        console.log("please write something in your list");
+    }
+    todos.push(todosquestions.firstQuestion);
+    console.log(todos);
+    condition = todosquestions.secondQuestion;
+}
